373d81bdfe97b4b311586808cd2b58fe95c9b6e8
whoisseth <utkarshseth2019@gmail.com>
2024-02-06 00:18:32 +0530

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,162 @@
+/** @format */
+"use client";
+
 import Image from "next/image";
+import { FaCirclePlay, FaCirclePause } from "react-icons/fa6";
+import { GrPowerReset } from "react-icons/gr";
+import { useState, useEffect } from "react";
+import { MdMusicNote } from "react-icons/md";
+import { MdMusicOff } from "react-icons/md";
 
 export default function Home() {
+  const [isAudioMute, setAudioMute] = useState(false);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinute] = useState(0);
+  const [seconds, setSeconds] = useState(10);
+  const [isActive, setSetActive] = useState(false);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    // timer end
+    if (isActive && hours === 0 && minutes === 0 && seconds === 0) {
+      const audio = new Audio(
+        "/audio/mixkit-software-interface-start-2574.mp3"
+      );
+
+      isAudioMute && audio.play();
+
+      console.log("time complted");
+      setSetActive(false);
+    }
+    function decrementTime() {
+      if (hours === 0 && minutes === 0 && seconds === 0) {
+        console.log("time complted");
+        clearTimeout(timeout);
+        setSetActive(false);
+      } else {
+        console.log("else condtion");
+        setSeconds((preSeconds) => (preSeconds === 0 ? 59 : preSeconds - 1));
+        if (seconds === 0) {
+          setMinute((preMinutes) => (preMinutes === 0 ? 59 : preMinutes - 1));
+
+          if (minutes === 0) {
+            setHours((preHours) => Math.max(0, preHours - 1));
+          }
+        }
+      }
+    }
+
+    if (isActive && (hours > 0 || minutes > 0 || seconds > 0)) {
+      timeout = setTimeout(decrementTime, 1000);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [hours, minutes, seconds, isActive]);
+
+  function toggleTimer() {
+    const audio = new Audio("/audio/mixkit-alert-quick-chime-766.mp3");
+
+    isAudioMute && audio.play();
+
+    setSetActive(!isActive);
+  }
+
+  function handleResetTimer() {
+    setSetActive(false);
+    setHours(0);
+    setMinute(0);
+    setSeconds(10);
+  }
+
+  const disableStartBtn = hours === 0 && minutes === 0 && seconds === 0;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex bg-slate-800 text-white min-h-screen items-center justify-center p-8 flex-col">
+      <button
+        onClick={() => setAudioMute(!isAudioMute)}
+        className=" fixed top-10 left-10 text-5xl "
+      >
+        {isAudioMute ? <MdMusicNote /> : <MdMusicOff />}
+      </button>
+      {/* min */}
+      {/* sec */}
+
+      {/* timers */}
+      <main className="flex flex-col  gap-8 ">
+        <section className="flex gap-5  text-6xl">
+          {/* hrs */}
+          <div className="flex items-start gap-1 flex-col ">
+            <select
+              value={hours}
+              onChange={(e) => setHours(Number(e.target.value))}
+              className="bg-inherit  w-auto outline-none cursor-pointer  hover:opacity-80 hover:scale-105 transition-all"
+            >
+              {Array.from({ length: 24 }).map((_, i) => (
+                <option className="" key={i} value={i}>
+                  {i}
+                </option>
+              ))}
+            </select>
+            <p className="text-sm ml-1 ">h</p>
+          </div>
+          {/* min */}
+
+          <div className="flex items-start gap-1 flex-col ">
+            <select
+              value={minutes}
+              onChange={(e) => setMinute(Number(e.target.value))}
+              className="bg-inherit  w-auto outline-none cursor-pointer  hover:opacity-80 hover:scale-105 transition-all"
+            >
+              {Array.from({ length: 60 }).map((_, i) => (
+                <option className="" key={i} value={i}>
+                  {i}
+                </option>
+              ))}
+            </select>
+            <p className="text-sm ml-1 ">m</p>
+          </div>
+
+          {/* sec */}
+          <div className="flex items-start gap-1 flex-col ">
+            <select
+              value={seconds}
+              onChange={(e) => setSeconds(Number(e.target.value))}
+              className="bg-inherit  w-auto outline-none cursor-pointer  hover:opacity-80 hover:scale-105 transition-all"
+            >
+              {Array.from({ length: 60 }).map((_, i) => (
+                <option className="" key={i} value={i}>
+                  {i}
+                </option>
+              ))}
+            </select>
+            <p className="text-sm ml-1 ">s</p>
+          </div>
+        </section>
+
+        {/* buttuons */}
+        <section className="flex gap-16 text-4xl ">
+          <button
+            onClick={handleResetTimer}
+            className=" hover:opacity-80 hover:scale-105 transition-all"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <GrPowerReset />
+          </button>
+          <button
+            className={` hover:opacity-80 hover:scale-105 transition-all ${
+              disableStartBtn && "cursor-not-allowed"
+            }  `}
+            disabled={disableStartBtn}
+            onClick={toggleTimer}
+          >
+            {isActive ? <FaCirclePause /> : <FaCirclePlay />}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            {/* <FaCirclePause /> */}
+          </button>
+          <div />
+        </section>
+      </main>
+    </div>
   );
 }
